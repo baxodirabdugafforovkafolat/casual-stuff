6d83c1e2d31523e3625acb3d64a1c8f864de044c
baxodirabdugafforovkafolat <baxodirabdugafforovkafolat@gmail.com>
2026-08-21 16:53:14 +0500

    first commit

--- obj/Debug/net10.0/godot.AssemblyInfo.cs
+++ obj/Debug/net10.0/godot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("godot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e00d4a0d54e9775663a5ec78133a3b610962d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("godot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("godot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
